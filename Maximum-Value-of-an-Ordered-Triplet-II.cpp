598a49e4ec5b074eb77a04246d02b4ed67d19133
AnmolManghera <55028848+AnmolManghera@users.noreply.github.com>
2025-04-03 10:59:07 +0530

    [2874] [Time Beats: 100.00%] [Memory Beats: 91.09%] - LeetPush

--- Maximum-Value-of-an-Ordered-Triplet-II.cpp
+++ Maximum-Value-of-an-Ordered-Triplet-II.cpp
@@ -3,23 +3,19 @@ public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
         long long ans = 0;
-        vector<int>rangeDiffMax(n,-1);
+        int maxRangeDiff = 0;
         int maxNo = nums[0];
         for(int i = 1 ; i < n ; i++) {
+            if(i >= 2) {
+                if(maxRangeDiff != -1) {
+                    ans = max(ans, 1LL*maxRangeDiff*nums[i]);
+                }
+            }
             if(maxNo - nums[i] >= 0) {
-                rangeDiffMax[i] = maxNo - nums[i];
+                maxRangeDiff = max(maxNo - nums[i], maxRangeDiff);
             }
             maxNo = max(nums[i],maxNo);
-        }
-        
-        maxNo = nums[n-1];
-        for(int i = n-2 ; i >= 1 ; i--) {
-            if(rangeDiffMax[i] != -1) {
-                ans = max(ans, 1LL*rangeDiffMax[i]*maxNo);
-            }
-            maxNo = max(nums[i],maxNo);
-        }
-        
+        }    
         return ans;
     }
 };
